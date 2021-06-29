0aae03164b7fa61f650b919daecdd25ffa175f9d
Nathaniel <nfgolding@gmail.com>
2021-06-29 15:10:44 +0200

    added invoke command to win and crash

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float invokeDelay = 1.0f;
     private void OnCollisionEnter(Collision other) {
         switch (other.gameObject.tag)
         {
@@ -16,20 +17,52 @@ public class CollisionHandler : MonoBehaviour
 
             case "Finish":
                 Debug.Log("You Won!");
+                StartNextLevelSequence();
                 break;
 
             default:
-                Debug.Log("Collided with something else");
-                ReloadLevel();
+                // Debug.Log("Collided with something else");
+                StartCrashSequence();
                 break;
         }
+    }
+
+    void StartCrashSequence(){
+        //disable movement component
+        GetComponent<Movement>().enabled = false;
+        // TODO: add SFX for crash
+        // TODO: add particle effects for crash
+        //Reload level
+        Invoke("ReloadLevel", invokeDelay);
+    }
+
+    void StartNextLevelSequence(){
+        //disable movement component
+        GetComponent<Movement>().enabled = false;
+
+        // TODO: add SFX for win        
+        // TODO: add particle effects for crash
+
+        //load next level level
+        Invoke("LoadNextLevel", invokeDelay);
     }
 
     void ReloadLevel(){
         // SceneManager.LoadScene("Sandbox");
 
-    
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // current scene index for the 'Level'
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel(){
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // current scene index for the 'Level'
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // If last level, reset game
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
